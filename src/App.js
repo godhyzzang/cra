5e438a858641065af4730198a447f48360d42879
hana <amksh0914@gmail.com>
2024-07-14 20:05:35 +0900

    recentMentors,mentors 레이아웃만 구현

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./css/App.css";
-import { TestButton, RadSquareCard } from "./css/styled.js";
+import MentorsList from "./components/Mentors/MentorsList";
+import "./css/App.css"; // 스타일을 적용하기 위한 기본 CSS 파일 (필요에 따라 설정)
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <TestButton radius={15}>빨간버튼</TestButton> {/* 동적 border-radius */}
-      </header>
-      <RadSquareCard
-        width={100}
-        height={150}
-        radius={15}
-        text="둥근 카드 테스트"
-      />
+      <MentorsList />
     </div>
   );
 }
